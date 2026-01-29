d48fb7d4260d304675c70c707859bea7821d3d3e
sleewga <matej.plum7@proton.me>
2026-01-29 22:41:48 +0100

    Add rnd num generator and input form

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
-const startGameBtn = document.querySelector('#start-game')
-const gameSection = document.querySelector('#game')
-const gameForm = document.querySelector('#game-form')
+const startGameBtn = document.querySelector("#start-game");
+const gameSection = document.querySelector("#game");
+const gameForm = document.querySelector("#game-form");
 let rndNumber;
 
-document.addEventListener('click', () => {
-    if (event.target.id == 'start-game'){
-        startGame();
-    }
-})
+document.addEventListener("click", () => {
+  if (event.target.id == "start-game") {
+    startGame();
+  }
+});
 
-function startGame(){
-    rndNumber = Math.random(1, 100);
+function startGame() {
+  rndNumber = Math.random(1, 100);
 
-    startGameBtn.classList.toggle('hidden');
-    gameSection.classList.toggle('hidden');
+  startGameBtn.classList.toggle("hidden");
+  gameSection.classList.toggle("hidden");
 }
